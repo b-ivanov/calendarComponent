83fabe51fd19193ac0aeee6efaa2f7a3c5d39497
b-ivanov <bojidar.g.i@gmail.com>
2021-06-10 21:57:07 +0300

    Added month change reactivity

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -2,36 +2,34 @@ import React from 'react';
 import DayHeaders from './DayHeaders';
 import DayCell from './DayCell';
 import CalendarComponentProps from '../interfaces/CalendarComponentProps';
+import CalendarComponentState from '../interfaces/CalendarComponentState';
 import DayCellProps from '../interfaces/DayCellProps';
 
 /**The CalendarComponent component renders the outer shell of the table */
-class CalendarComponent extends React.Component <CalendarComponentProps> {
-	year:number;
-	month:number;
-	day:number;
-	dateObject:Date;
-
+class CalendarComponent extends React.Component <CalendarComponentProps, CalendarComponentState> {
 	constructor (props:CalendarComponentProps) {
 		super(props);
 		const now:Date = new Date();
-		this.year = (props.startingDate ? props.startingDate.year : now.getFullYear());
-		this.month = (props.startingDate ? props.startingDate.month : now.getMonth());
-		this.day = (props.startingDate ? props.startingDate.day : now.getDate());
-		this.dateObject = new Date(this.year, this.month, this.day);
-	}
+		this.state = {
+			year: (props.startingDate ? props.startingDate.year : now.getFullYear()),
+			month: (props.startingDate ? props.startingDate.month : now.getMonth()),
+			day: (props.startingDate ? props.startingDate.day : now.getDate())
+		};
+	};
 
 	getMonthNameAndYear ():string {
-		const monthName = this.dateObject.toLocaleString('default', { month: 'long' });
-		return monthName + " " + this.year;
+		const date:Date = new Date(this.state.year, this.state.month, this.state.day);
+		const monthName = date.toLocaleString('default', { month: 'long' });
+		return monthName + " " + this.state.year;
 	};
 
 	getMonthObject ():DayCellProps[] {
-		const firstDayIndex:number = (new Date(this.year, this.month, 1).getDay() - 1);
-		const finalDay:number = (32 - new Date(this.year, this.month, 32).getDate());
-		const finalDayPrevMonth:number = (32 - new Date(this.year, (this.month - 1), 32).getDate());
-		console.log("finalDayPrevMonth", finalDayPrevMonth, firstDayIndex);
+		const firstDayIndex:number = (new Date(this.state.year, this.state.month, 1).getDay() - 1);
+		const finalDay:number = (32 - new Date(this.state.year, this.state.month, 32).getDate());
+		const finalDayPrevMonth:number = (32 - new Date(this.state.year, (this.state.month - 1), 32).getDate());
 		const outObject:DayCellProps[] = [];
 		const numOfCells:number = 35;
+		console.log("firstDayIndex", firstDayIndex);
 		for (let i = (1 - firstDayIndex); i <= (numOfCells - firstDayIndex); i++) {
 			if (i < 1) { //previous month cells
 				outObject.push({
@@ -52,11 +50,42 @@ class CalendarComponent extends React.Component <CalendarComponentProps> {
 		}
 		return outObject;
 	};
+
 	renderWholeMonth (mntObj:DayCellProps[]):Element[] {
 		return mntObj.map((dayObj:DayCellProps, index:number):any => {
 			return <DayCell dayNumber={dayObj.dayNumber} isFromCurrentMonth={dayObj.isFromCurrentMonth} key={"dc_" + index}/>;
 		});
-	}
+	};
+	
+	incrementMonth ():void {
+		let tempMonth:number = this.state.month;
+		let tempYear:number = this.state.year;
+		tempMonth++;
+		if (tempMonth > 11) {
+			tempMonth = 0;
+			tempYear++;
+		}
+		this.setState({
+			year: tempYear,
+			month: tempMonth,
+			day: this.state.day
+		});
+	};
+
+	decrementMonth ():void {
+		let tempMonth:number = this.state.month;
+		let tempYear:number = this.state.year;
+		tempMonth--;
+		if (tempMonth < 0) {
+			tempMonth = 11;
+			tempYear--;
+		}
+		this.setState({
+			year: tempYear,
+			month: tempMonth,
+			day: this.state.day
+		});
+	};
 	/**Component render function */
 	render () {
 		const monthObj:DayCellProps[] = this.getMonthObject();
@@ -66,8 +95,8 @@ class CalendarComponent extends React.Component <CalendarComponentProps> {
 				<div className="subHeader">
 					<div className="monthName">{this.getMonthNameAndYear()}</div>
 					<div className="navigation">
-						<span>&#10094;</span>
-						<span>&#10095;</span>
+						<span onClick={() => this.decrementMonth()}>&#10094;</span>
+						<span onClick={() => this.incrementMonth()}>&#10095;</span>
 					</div>
 				</div>
 				<ul className="monthGrid">
@@ -76,7 +105,7 @@ class CalendarComponent extends React.Component <CalendarComponentProps> {
 				</ul>
 			</div>
 		);
-	}
+	};
 };
 
 export default CalendarComponent;
---
+++ src/interfaces/CalendarComponentState.ts
@@ -0,0 +1,5 @@
+export default interface CalendarComponentState {
+	year: number;
+	month: number;
+	day: number;
+};
